0a57458bb6765b0132868ddd89843bda3b1ca6ea
arshvinw5 <tarshvin@gmail.com>
2025-12-31 14:08:59 +0530

    add blog entities

---
+++ lib/features/blog/domain/entities/blog.dart
@@ -0,0 +1,19 @@
+class Blog {
+  final String id;
+  final String posterId;
+  final String title;
+  final String content;
+  final String imageUrl;
+  final List<String> categories;
+  final DateTime updatedAt;
+
+  Blog({
+    required this.id,
+    required this.posterId,
+    required this.title,
+    required this.content,
+    required this.imageUrl,
+    required this.categories,
+    required this.updatedAt,
+  });
+}
