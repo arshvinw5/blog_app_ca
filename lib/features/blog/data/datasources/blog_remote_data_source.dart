f1ce3b0e3b399cb3cf7d09102ecb655609bb81ff
arshvinw5 <tarshvin@gmail.com>
2026-01-02 12:49:57 +0530

    add usercase and repository impl

--- lib/features/blog/data/datasources/blog_remote_data_source.dart
+++ lib/features/blog/data/datasources/blog_remote_data_source.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+
 import 'package:ca_blog_app/core/error/exceptions.dart';
 import 'package:ca_blog_app/features/blog/data/models/blog_models.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class BlogRemoteDataSource {
   Future<BlogModel> uploadBlog(BlogModel blog);
+  Future<String> uploadImage({
+    required File imageFile,
+    required BlogModel blog,
+  });
 }
 
 class BlogRemoteDataSourceImpl implements BlogRemoteDataSource {
@@ -21,7 +27,32 @@ class BlogRemoteDataSourceImpl implements BlogRemoteDataSource {
           .single();
 
       //cover the json to blog model
+      //purpose of converting this to map is get the url from the supabase
       return BlogModel.fromMap(blogData);
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+
+  @override
+  Future<String> uploadImage({
+    required File imageFile,
+    required BlogModel blog,
+  }) async {
+    try {
+      //to upload image to supabase storage
+      await supabaseClient.storage
+          .from('blog_images')
+          .upload(blog.id, imageFile);
+
+      //to advance =>
+      //upload the images to separate folder inside the storage
+      //:upload(${blog.id}/images, imageFile);
+
+      //${blog.id}-> folder name
+
+      //to retrieve the public url of the image
+      return supabaseClient.storage.from('blog_images').getPublicUrl(blog.id);
     } catch (e) {
       throw ServerException(e.toString());
     }
--- lib/features/blog/data/models/blog_models.dart
+++ lib/features/blog/data/models/blog_models.dart
@@ -37,4 +37,24 @@ class BlogModel extends Blog {
           : DateTime.parse(map['updated_at'] as String),
     );
   }
+
+  BlogModel copyWith({
+    String? id,
+    String? posterId,
+    String? title,
+    String? content,
+    String? imageUrl,
+    List<String>? categories,
+    DateTime? updatedAt,
+  }) {
+    return BlogModel(
+      id: id ?? this.id,
+      posterId: posterId ?? this.posterId,
+      title: title ?? this.title,
+      content: content ?? this.content,
+      imageUrl: imageUrl ?? this.imageUrl,
+      categories: categories ?? this.categories,
+      updatedAt: updatedAt ?? this.updatedAt,
+    );
+  }
 }
